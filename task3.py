2e8f56f90c812412cc6e371919d22400a86b9526
Jeromeg282 <113056411+Jeromeg282@users.noreply.github.com>
2023-10-03 23:23:27 -0700

    .

--- task3.py
+++ task3.py
@@ -1,11 +1,12 @@
+import random
 class NPC:
     stats = {
         'str' : 0,
-         'int' : 0,
-          'pie' : 0,
-           'agi' : 0,
-            'stm' : 0,
-             'cha' : 0
+        'int' : 0,
+        'pie' : 0,
+        'agi' : 0,
+        'stm' : 0,
+        'cha' : 0
           }
     level = 0
     hp = 0
@@ -14,8 +15,46 @@ class NPC:
     copper = 0
 
     def __init__(self):
+        self.stats['str'] = sum(random.randint(1, 6) for _ in range(3))
+        self.stats['int'] = sum(random.randint(1, 6) for _ in range(3))
+        self.stats['pie'] = sum(random.randint(1, 6) for _ in range(3))
+        self.stats['agi'] = sum(random.randint(1, 6) for _ in range(3))
+        self.stats['stm'] = sum(random.randint(1, 6) for _ in range(3))
+        self.stats['cha'] = sum(random.randint(1, 6) for _ in range(3))
 
+        levelgen=random.choices([1,2,3,4], weights=[40,30,20,10][0])
+        self.level=levelgen
+        self.hp=random.randint(1,10) * self.level
 
+        if random.random() <0.3:
+            self.gold=random.randint(0,6)
+        else:
+            self.silver=random.randint(3, 12)
+            if self.gold == 0 and random.random() < 0.75:
+                self.copper=random.randint(4,20)
+
+        self.wealth()
+    def wealth(self):
+        self.gold=0
+        self.silver=0
+        self.copper+=self.silver * 10
+        self.copper+=self.gold * 10 * 10
 
         return
     
+npcs = [NPC()for _ in range(100)]
+
+
+
+#Rules: 
+# each primary stat is generated as 3 random dice rolls (random 1-6) (strength, intelligance, piety, agility, stamina, charm)
+# distribution of level is 1: 40%, 2: 30%, 3: 20%, 4: 10%
+# each npc gets random 1-10 hp per level
+# each npc gets some random money
+# 30% chance to have 0-6 gold
+# 50% chance go have 3-12 silver
+# if they had no gold, they have a 75% chance to have 4-20 copper
+# each gold is worth 10 silver and each silver is worth 10 copper
+
+
+
